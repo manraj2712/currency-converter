e76b3c1a511be1ce2992021f6319337ff7de3dc6
Manraj <manraj2712@gmail.com>
2023-12-31 05:15:31 +0530

    added vercel settings

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(301).redirect("/api");
+});
+
 app.get("/api", (req, res) => {
   res.status(200).json("Welcome to the Crypto Converter API");
 });
