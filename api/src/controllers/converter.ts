8e3de86c81aba41067d1b176abb92cec1a80ffee
Manraj <manraj2712@gmail.com>
2023-12-31 06:34:09 +0530

    added api documentation

--- api/src/controllers/converter.ts
+++ api/src/controllers/converter.ts
@@ -2,6 +2,40 @@ import { CustomError, coingeckoHeaders } from "../utils";
 import { BigPromise } from "../middlewares";
 import axios from "axios";
 
+/**
+ * @swagger
+ * /api/convert/{from}/{to}/{amount}:
+ *   get:
+ *     summary: Convert crypto to other currencies
+ *     description: Convert crypto to other supported currencies
+ *     parameters:
+ *       - in: path
+ *         name: from
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The crypto to convert from
+ *       - in: path
+ *         name: to
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The crypto to convert to
+ *       - in: path
+ *         name: amount
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The amount to convert
+ *     responses:
+ *       '200':
+ *         description: Success
+ *       '400':
+ *         description: Bad Request
+ *       '500':
+ *         description: Internal Server Error
+ */
+
 export const convertTokenToCurrencyController = BigPromise(
   async (req, res, next) => {
     const { from, to, amount } = req.params;
@@ -23,6 +57,19 @@ export const convertTokenToCurrencyController = BigPromise(
     });
   }
 );
+
+/**
+ * @swagger
+ * /api/get-currencies:
+ *   get:
+ *     summary: Get top 100 coins and supported currencies
+ *     description: Get top 100 taken by market cap coins and supported currencies
+ *     responses:
+ *       '200':
+ *         description: Success
+ *       '500':
+ *         description: Internal Server Error
+ */
 
 export const getCurrenciesController = BigPromise(async (req, res, next) => {
   const response = await Promise.all([
--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import converterRouter from "./routes/converter";
 import { ErrorHandler } from "./middlewares";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 dotenv.config();
 const app = express();
@@ -13,9 +15,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(301).redirect("/api");
-});
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Crypto Converter API",
+      version: "1.0.0",
+      description: "A simple crypto converter API",
+    },
+    servers: [
+      {
+        url: "https://currency-converter-gtp4jrn7b-manrajs-projects.vercel.app",
+        name: "Production",
+      },
+      {
+        url: "http://localhost:8080",
+        name: "Development",
+      },
+    ],
+    "x-server-names": ["Production", "Development"],
+  },
+  apis: ["./src/controllers/*.ts"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/api", (req, res) => {
   res.status(200).json("Welcome to the Crypto Converter API");
